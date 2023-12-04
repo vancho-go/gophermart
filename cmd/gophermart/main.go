0c5ba23abdfd02e9f96d8d41e6c1e16e6f9ed128
Ivan Goncharov <goncharov.80@mail.ru>
2023-12-04 13:41:16 +0300

    Router prototype

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import "github.com/go-chi/chi/v5"
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Route("/api/user", func(r chi.Router) {
+		r.Group(func(r chi.Router) {
+			r.Post("/register", nil)
+			r.Post("/login", nil)
+			r.Post("/orders", nil)
+			r.Get("/orders", nil)
+			r.Get("/withdrawals", nil)
+		})
+
+		r.Route("/balance", func(r chi.Router) {
+			r.Group(func(r chi.Router) {
+				r.Get("/", nil)
+				r.Post("/withdraw", nil)
+			})
+		})
+	})
+}
