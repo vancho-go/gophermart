c1b593c4be25b1e5f000003dae836d093942ca0b
Ivan Goncharov <goncharov.80@mail.ru>
2023-12-28 11:36:36 +0300

    Fixed accrual data type

--- internal/app/models/models.go
+++ internal/app/models/models.go
@@ -20,7 +20,7 @@ type APIAddOrderRequest struct {
 type APIGetOrderResponse struct {
 	Number     string    `json:"number"`
 	Status     string    `json:"status"`
-	Accrual    *int      `json:"accrual,omitempty"`
+	Accrual    *float64  `json:"accrual,omitempty"`
 	UploadedAt time.Time `json:"uploaded_at"`
 }
 
